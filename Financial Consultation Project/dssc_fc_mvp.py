db1ebf03f7ae0f66c67e0d600cea1f850d21a7b4
Twofootwalkingdisaster <zianjohn14@gmail.com>
2022-10-19 17:11:58 -0400

    Most of the files have been refactored and corrected...

---
+++ Financial Consultation Project/dssc_fc_mvp.py
@@ -0,0 +1,15 @@
+import dssc_fc_mvp_storage as storage_information
+
+
+# The main start point of the project is this function
+def main():
+    storage_information.InitializeDSSCService.initialize_dssc_service()
+
+
+# START POINT OF THE PROGRAM
+if __name__ == "__main__":
+    main()
+
+
+
+
--- Financial Consultation Project/dssc_fc_mvp_storage.py
+++ Financial Consultation Project/dssc_fc_mvp_storage.py
@@ -4,19 +4,20 @@
 # The storage we have used in this Amazon Web Services S3 \
 # it's a simple cloud storage that provides object storage through a web service interface
 
-from dssc_fc_mvp_storage_config import StorageConfig as storage_config
+
+from io import BytesIO
 import boto3
 import pandas as pd
 import os
-from io import BytesIO
 import json
 
+# Maintain a global instance of the configuration file
+storage_config = None
 
 # Maintain a global instance of the client variable
 s3_client = None
 
 
-# This class will be responsible to connect with the AWS S3 database and extract the data
 class ConnectToS3DataBase:
     # Here this method will initiate the connection request to the S3 database
     @staticmethod
@@ -26,19 +27,21 @@ class ConnectToS3DataBase:
         # This Null check is done to prevent any crash issue that is caused by null values
         if s3_client is None:
             s3_client = boto3.client('s3',
-                                     aws_access_key_id=storage_config.AWS_ACCESS_KEY_ID,
-                                     aws_secret_access_key=storage_config.AWS_SECRET_ACCESS_KEY)
+                                     aws_access_key_id=storage_config['aws_access_key_id'],
+                                     aws_secret_access_key=storage_config['aws_secret_access_key'])
+
+            print(f'Client authentication has been created {s3_client}')
 
 
 # This class is responsible to fetch the .csv data files from S3 Database
-class FetchFilesFromS3Database:
+class S3DatabaseActions:
     # This method below will browse through the directory in the local system and \
     # update the S3 database
     @staticmethod
     def update_s3_database():
         for file in os.listdir():
-            upload_file_bucket = storage_config.AWS_BUCKET_NAME
-            upload_file_key = storage_config.AWS_BUCKET_SUB_FOLDER_1
+            upload_file_bucket = storage_config['aws_bucket_name']
+            upload_file_key = storage_config['aws_bucket_sub_folder_1']
             s3_client.upload_file(file,
                                   upload_file_bucket,
                                   upload_file_key)
@@ -46,24 +49,28 @@ class FetchFilesFromS3Database:
     # This method below will get the csv file for a specific company from S3 database
     @staticmethod
     def fetch_file_from_s3_database(company):
-        file = s3_client.get_object(Bucket=storage_config.AWS_BUCKET_NAME,
-                                    Key=storage_config.AWS_BUCKET_SUB_FOLDER_1+'/'+company+'_data.csv')
+        file = s3_client.get_object(Bucket=storage_config['aws_bucket_name'],
+                                    Key=storage_config['aws_bucket_sub_folder_1']+'/'+company+'_data.csv')
         read_file = file['Body'].read()
         read_file_csv = pd.read_csv(BytesIO(read_file))
 
         # return the read file
         return read_file_csv
 
-
-ConnectToS3DataBase.client_connection_authentication()
-Data = FetchFilesFromS3Database.fetch_file_from_s3_database('Apple')
-print(Data)
+    # This function will pull the data for a particular ticker symbol
 
 
+# This class here initializes all the services for the start of the project
+class InitializeDSSCService:
+    # The static method below initializes all services
+    @staticmethod
+    def initialize_dssc_service():
 
+        # Load the created configuration file
+        configuration_file = open('dssc_fc_mvp_configuration.json', 'r')
+        global storage_config
+        storage_config = json.load(configuration_file)
+        print(storage_config)
 
-
-
-
-
-
+        # Run the client authentication method
+        ConnectToS3DataBase.client_connection_authentication()
