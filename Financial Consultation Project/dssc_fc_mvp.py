423ae0a25b69d0256066fb3f551eed31b653aa93
Twofootwalkingdisaster <zianjohn14@gmail.com>
2022-11-23 13:43:04 -0500

    Fixed the following codes : 1. The file upload to s3 bucket is now successful 2. The temp files are created automatically if they don't exist 3. The temp data set files are removed after

--- Financial Consultation Project/dssc_fc_mvp.py
+++ Financial Consultation Project/dssc_fc_mvp.py
@@ -1,9 +1,16 @@
+import pandas as pd
 import dssc_fc_mvp_storage as storage_information
 
 
 # The main start point of the project is this function
 def main():
+    storage_information.Create_Local_Files.create_local_file()
     storage_information.InitializeDSSCService.initialize_dssc_service()
+    company_list = ()
+
+    # The code below updates all the files with the latest stock data till date
+    update_file, file_name = storage_information.S3DatabaseActions.update_files_to_latest_data("BMO")
+    storage_information.S3DatabaseActions.update_s3_database(update_file, file_name)
 
 
 # START POINT OF THE PROGRAM
